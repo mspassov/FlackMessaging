0b74a300639fa03d8a457e1edcbf50475b2a65ba
Martin Spassov <martispassov@gmail.com>
2020-05-26 12:35:08 -0400

    added room functionality + multiple user setup

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 import os
+import requests
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -11,3 +12,18 @@ socketio = SocketIO(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+	username = request.form.get("user")
+	channel = request.form.get("channel")
+	return render_template("room.html", username=username, channel=channel)
+
+@socketio.on('joined_room')
+def joinEvent(data):
+	join_room(data['channel'])
+	socketio.emit('new_user_announce', data)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
